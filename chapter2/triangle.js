0b57d4bcf53cae647bf59c78d2de6eeda353dea8
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-17 19:51:46 -0300

    Adding solution to looping a triangle.

---
+++ chapter2/triangle.js
@@ -0,0 +1,13 @@
+/**
+ * Solutions to Eloquent js exercises.
+ *
+ * Chapter 2
+ * Looping a triangle
+ */
+
+var hashes = '';
+for (var i = 0; i < 7; i++) {
+    hashes += '#';
+    console.log(hashes);
+};
+
