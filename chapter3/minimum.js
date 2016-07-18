19cdae17c5153e9d91e6898eb8ff95e0cdce7c59
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-18 00:14:22 -0300

    adding solution for minimum exercise

---
+++ chapter3/minimum.js
@@ -0,0 +1,16 @@
+/**
+ * Solutions to Eloquent js exercises.
+ *
+ * Chapter 3
+ * Minimum
+ */
+
+/**
+ * Returns the smallest number.
+ * @param  Number x
+ * @param  Number y
+ * @return Number
+ */
+function min(x, y) {
+  return x > y ? y : x;
+}
